e69cfda0f8c2c8d85b5d5ed61f06df7165c34d2e
battle_angel <six@angel>
2025-06-06 18:00:04 -0400

    th freezetimer ini

---
+++ tonyhawk freezetimer/THfreezetimer/th-ft-program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CS8981 
+namespace freezetimer
+{
+  class program
+
+  {
+    static void Main()
+    {
+      titler.generator.print("trump");
+    }
+  }
+}
